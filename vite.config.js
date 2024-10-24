a3ccd3f71ce86791a379e1f17b3ebc79174e700f
moriyam-mohona <moriyammohona@gmail.com>
2024-10-24 11:12:17 +0600

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   // base: "/E-Learning/",
+//   plugins: [react()],
+// });
+
+// vite.config.js
+// export default {
+//   base: '/your-repo-name/', // replace 'your-repo-name' with the actual repo name
+// }
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// Use the base property for production only
 export default defineConfig({
+  base: process.env.NODE_ENV === "production" ? "/E-Learning/" : "/", // Adjusts based on the environment
   plugins: [react()],
-})
+});
