6e89918555064e36a4ff351fb9449a5c7fb9fdc3
moriyam-mohona <moriyammohona@gmail.com>
2024-10-24 11:32:19 +0600

    Edit: Error Page

--- src/Components/Pages/ErrorPage/ErrorPage.jsx
+++ src/Components/Pages/ErrorPage/ErrorPage.jsx
@@ -1,17 +1,21 @@
+import { useRouteError } from "react-router-dom";
 import { Link } from "react-router-dom";
 import vector from "../../../assets/ErrorVector.png";
-const ErrorPage = () => {
+export default function ErrorPage() {
+  const error = useRouteError();
+  console.error(error);
+
   return (
-    <div className="bg-blue-300/10 min-h-screen flex flex-col justify-center items-center p-5">
-      <img src={vector} alt="" className="sm:max-w-sm md:max-w-lg" />
-      <Link
-        to="/"
-        className="bg-blue-300/20 border-b-4 border-r-4 border-b-blue-300 border-r-blue-300 p-3 rounded-lg text-blue-300 font-bold w-full text-center sm:max-w-sm md:max-w-lg hover:scale-105"
-      >
-        Go Back To Home{" "}
-      </Link>{" "}
+    <div id="error-page">
+      <div className="bg-blue-300/10 min-h-screen flex flex-col justify-center items-center p-5">
+        <img src={vector} alt="" className="sm:max-w-sm md:max-w-lg" />
+        <Link
+          to="/"
+          className="bg-blue-300/20 border-b-4 border-r-4 border-b-blue-300 border-r-blue-300 p-3 rounded-lg text-blue-300 font-bold w-full text-center sm:max-w-sm md:max-w-lg hover:scale-105"
+        >
+          Go Back To Home{" "}
+        </Link>{" "}
+      </div>
     </div>
   );
-};
-
-export default ErrorPage;
+}
